7dd107fb19a63b97433d31511745c2a80d7b4faa
OUN123 <116722639+OUN123@users.noreply.github.com>
2024-03-25 23:01:03 +0100

    Add files via upload

--- build/run.sh
+++ build/run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "This script should start your application"
+echo "Building frontend and backend Docker images..."
+
+
+cd ../frontend
+docker build -t tic-tac-toe-frontend .
+
+
+cd ../backend
+docker build -t tic-tac-toe-backend .
+
+echo "Docker images built successfully."
